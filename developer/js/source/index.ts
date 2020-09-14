749dde25af8c6a0e17ec054b086af8f62b90d52d
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-14 12:03:38 -0600

    feat(developer): index for lexical model compiler

---
+++ developer/js/source/index.ts
@@ -0,0 +1,4 @@
+import LexicalModelCompiler from "./lexical-model-compiler/lexical-model-compiler";
+import {ModelSourceError} from "./lexical-model-compiler/lexical-model-compiler";
+
+export {LexicalModelCompiler, ModelSourceError};
