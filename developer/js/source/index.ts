dd237000d77a5cc51017716f4d675bdfa160bbd5
Eddie Antonio Santos <easantos@ualberta.ca>
2020-09-15 15:50:36 -0600

    feat(developer/compilers): export all the useful things

--- developer/js/source/index.ts
+++ developer/js/source/index.ts
@@ -1,4 +1,19 @@
-import LexicalModelCompiler from "./lexical-model-compiler/lexical-model-compiler";
-import {ModelSourceError} from "./lexical-model-compiler/lexical-model-compiler";
+/**
+ * NOTE: None of the members imported here (and subsequently exported) will use
+ * Node imports, therefore, they are safe to use in the Browser or WebWorker
+ * context.
+ */
+import LexicalModelCompiler, { DefaultLexicalModelCompiler, ModelSourceError } from "./lexical-model-compiler/lexical-model-compiler";
+import { LineNoAndText, WordList, WordListSource } from "./lexical-model-compiler/wordlist";
 
-export {LexicalModelCompiler, ModelSourceError};
+export {
+  /* Interfaces and types */
+  LexicalModelCompiler,
+  LineNoAndText,
+  WordList,
+  WordListSource,
+
+  /* Concrete implementations */
+  DefaultLexicalModelCompiler,
+  ModelSourceError,
+};
